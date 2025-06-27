885a1a9515f45c60f6a1c3d1c2acac08ee15de98
Venkata Sai <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>
2025-06-27 15:43:29 +0530

    Update EncryptedClusterWideMapUtil.java
    Signed-off-by: Venkata Sai <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>

--- src/main/java/jp/co/sony/csl/dcoes/apis/common/util/vertx/EncryptedClusterWideMapUtil.java
+++ src/main/java/jp/co/sony/csl/dcoes/apis/common/util/vertx/EncryptedClusterWideMapUtil.java
@@ -13,11 +13,12 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.vertx.core.shareddata.AsyncMap;
 import jp.co.sony.csl.dcoes.apis.common.util.EncryptionUtil;
 
@@ -436,11 +437,11 @@ public class EncryptedClusterWideMapUtil {
 					List<String> values = resValues.result();
 					@SuppressWarnings("rawtypes") List<Future> futures = new ArrayList<>(values.size());
 					for (String value : values) {
-						Future<V> future = Future.future();
-						decrypt_(value, future);
-						futures.add(future);
+						Promise<V> promise = Promise.promise();
+						decrypt_(value, promise);
+						futures.add(promise.future());
 					}
-					CompositeFuture.all(futures).setHandler(ar -> {
+					CompositeFuture.all(futures).onComplete(ar -> {
 						if (ar.succeeded()) {
 							List<V> result = new ArrayList<>(ar.result().size());
 							for (int i = ar.result().size(); 0 < i--;) {
@@ -468,11 +469,11 @@ public class EncryptedClusterWideMapUtil {
 					for (K key : entries.keySet()) keys.add(key);
 					@SuppressWarnings("rawtypes") List<Future> futures = new ArrayList<>(keys.size());
 					for (K key : keys) {
-						Future<V> future = Future.future();
-						decrypt_(entries.get(key), future);
-						futures.add(future);
+						Promise<V> promise = Promise.promise();
+						decrypt_(value, promise);
+						futures.add(promise.future());
 					}
-					CompositeFuture.all(futures).setHandler(ar -> {
+					CompositeFuture.all(futures).onComplete(ar -> {
 						if (ar.succeeded()) {
 							Map<K, V> result = new HashMap<>(ar.result().size());
 							for (int i = ar.result().size(); 0 < i--;) {
