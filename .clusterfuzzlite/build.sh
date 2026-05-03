2a593e38a8c129d277afd63d0d73b3e5ce5c9186
Flawz <rajiaminetebai@gmail.com>
2026-05-03 23:43:35 +0200

    Added ClusterFuzzLite to the codebase

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2026 The APIS Common Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# Build apis-common and a Jazzer fuzz target without modifying pom.xml.
-# Mirrors patterns from https://google.github.io/oss-fuzz/getting-started/new-project-guide/jvm-lang/
 
 PROJECT_ROOT="${SRC}/apis-common"
 # Pinned apis-bom (same ref as CI in .github/workflows/ci.yml)
--- fuzz/ApisCommonFuzzer.java
+++ fuzz/ApisCommonFuzzer.java
@@ -1,31 +1,13 @@
-// Copyright 2026 The APIS Common Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
-
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-
 import jp.co.sony.csl.dcoes.apis.common.util.DateTimeUtil;
 import jp.co.sony.csl.dcoes.apis.common.util.NumberUtil;
 import jp.co.sony.csl.dcoes.apis.common.util.StringUtil;
 import jp.co.sony.csl.dcoes.apis.common.util.vertx.JsonObjectUtil;
 
-/**
- * Jazzer entry point for OSS-Fuzz / ClusterFuzzLite. Not on the Maven test classpath;
- * compiled only by {@code .clusterfuzzlite/build.sh} inside the fuzzing image.
- */
+
 public class ApisCommonFuzzer {
 	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
 		switch (data.consumeInt(0, 5)) {
